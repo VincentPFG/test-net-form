afe1b92a9f1ad1ee7b190add2ee454e20cb131b5
VincentPFG <vincentgiraud@live.fr>
2019-12-27 13:09:56 +0100

    test2

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -2,8 +2,14 @@
 Layout
     v-form(netlify name='test1')
         input(name='contenu')
+
+    v-btn(@click='dialog = true')
+    v-dialog
+        v-form(netlify nae='test2')
 </template>
 
 <script lang='coffee'>
-
+export default
+    data: ->
+        dialog: null
 </script>
